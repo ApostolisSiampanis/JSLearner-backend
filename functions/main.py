a62a5d7bd442f1abbb01d9b4800835c27d6377ff
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-21 22:01:57 +0300

    create check_and_update_user_experience

--- functions/main.py
+++ functions/main.py
@@ -7,7 +7,7 @@ initialize_app()
 
 
 @db_fn.on_value_written(reference=r"/users/{uid}/experience_score", region="us-central1")
-def update_leaderboard(event):
+def change_user_score(event):
     """
     Triggered when a user's experience score is updated.
     """
@@ -15,10 +15,11 @@ def update_leaderboard(event):
     uid = event.params['uid']
 
     # Call the asynchronous function
-    asyncio.run(process_update(uid, new_score))
+    asyncio.run(update_leaderboard(uid, new_score))
+    asyncio.run(check_and_update_user_experience(uid, new_score))
 
 
-async def process_update(uid, new_score) -> Any:
+async def update_leaderboard(uid, new_score) -> Any:
     """
     Process the update to the leaderboard.
     """
@@ -45,6 +46,36 @@ async def process_update(uid, new_score) -> Any:
             print(f"Leaderboard updated for user {uid} with score {new_score}.")
         else:
             print(f"User {uid} not found in Firestore.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+async def check_and_update_user_experience(uid, new_score) -> Any:
+    """
+    Check if the user has reached a certain experience level.
+    """
+    try:
+        experience_level_ref = db.reference(f"users/{uid}/experience_level")
+        experience_level = experience_level_ref.get()
+
+        new_experience_level = experience_level
+
+        if experience_level is 'A_LOT_OF_EXPERIENCE':
+            if new_score < 1000:
+                new_experience_level = 'SOME_EXPERIENCE'
+        elif experience_level is 'SOME_EXPERIENCE':
+            if new_score < 300:
+                new_experience_level = 'NO_EXPERIENCE'
+            elif new_score >= 1400:
+                new_experience_level = 'A_LOT_OF_EXPERIENCE'
+        elif experience_level is 'NO_EXPERIENCE':
+            if new_score >= 600:
+                new_experience_level = 'SOME_EXPERIENCE'
+
+        if new_experience_level != experience_level:
+            experience_level_ref.set(new_experience_level)
+            print(f"User {uid} has reached {new_experience_level} level.")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
