3b562f4e3ad2e3c4379f7552207d332d56460677
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-21 17:44:50 +0300

    - small changes

--- functions/main.py
+++ functions/main.py
@@ -7,20 +7,20 @@ initialize_app()
 
 
 @db_fn.on_value_written(reference=r"/users/{uid}/experience_score", region="us-central1")
-async def update_leaderboard(event):
+def update_leaderboard(event):
     """
-        Triggered when a user's experience score is updated.
+    Triggered when a user's experience score is updated.
     """
     new_score = event.data.after
     uid = event.params['uid']
 
     # Call the asynchronous function
-    await process_update(uid, new_score)
+    asyncio.run(process_update(uid, new_score))
 
 
 async def process_update(uid, new_score) -> Any:
     """
-        Process the update to the leaderboard.
+    Process the update to the leaderboard.
     """
     try:
         # Fetch user details from Firestore
@@ -33,24 +33,16 @@ async def process_update(uid, new_score) -> Any:
             lastname = user_data.get('last_name', 'Unknown')
             initials = f"{firstname[0]}{lastname[0]}"
 
-            # Check if the user exists in the rankings
+            # Define the leaderboard entry
             leaderboard_ref = db.reference(f"leaderboard/{uid}")
-            existing_score = leaderboard_ref.get()
-
-            # Define the new leaderboard entry
             leaderboard_entry = {
                 'initials': initials,
                 'score': new_score
             }
 
-            if existing_score:
-                # Update the rankings in Realtime Database
-                leaderboard_ref.update(leaderboard_entry)
-                print(f"Leaderboard updated for user {uid} with score {new_score}.")
-            else:
-                # Set the ranking in Realtime Database
-                leaderboard_ref.set(leaderboard_entry)
-                print(f"Leaderboard entry created for user {uid} with score {new_score}.")
+            # Update the leaderboard entry in Realtime Database
+            leaderboard_ref.set(leaderboard_entry)
+            print(f"Leaderboard updated for user {uid} with score {new_score}.")
         else:
             print(f"User {uid} not found in Firestore.")
 
